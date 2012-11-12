ea979111d5592770523487a1243d1b94ba11c947
Darrin Machay <darrin@machay.com>
2012-11-12 11:08:32 -0600

    initial commit

---
+++ .bash_profile
@@ -0,0 +1,10 @@
+
+alias ls='ls -G'
+
+# color terminal
+export CLICOLOR=1
+
+# startup virtualenv-burrito
+if [ -f $HOME/.venvburrito/startup.sh ]; then
+    . $HOME/.venvburrito/startup.sh
+fi
---
+++ relink.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Symlink all $repodir dotfiles in $linkdest and remove any
+# broken $linkdest (dotfile) links.
+
+# This is used to manage the git dotfile repo; run this script
+# after adding or removing dotfiles to $repodir to automatically
+# add/remove the symlinks in $linkdest. 
+# - Files that don't start with a dot in $dotfiles are ignored. 
+# - Files in $ignore_list are ignored (.git should be there)
+# - Existing links and "real" dotfiles are not changed for safety. 
+# - Broken dotfile links in $linkdest are deleted.
+
+repodir=~/dotfiles
+linkdest=~
+ignore_list=( .git )
+
+# Create links
+for f in $(ls -1A $repodir | egrep -e "^\."); do
+	for igf in ${ignore_list[@]}; do
+		if [ $igf == $f ]; then
+			continue 2
+		fi
+	done
+	
+	if [ -e $linkdest/$f ]; then
+		if [ ! -L $linkdest/$f ]; then
+			echo "Not changing existing (non-symlink) file: $linkdest/f"
+		fi
+		# Silently ignore existing symlinks
+		continue
+	fi
+	
+	echo "Creating link $linkdest/$f -> $repodir/$f"
+	ln -s $repodir/$f $linkdest/$f
+	
+done
+
+# Delete broken links
+for f in $(ls -1A $linkdest | egrep -e "^\."); do
+	if [ -L $linkdest/$f -a ! -e $linkdest/$f ]; then
+		echo "Removing broken link $linkdest/$f"
+		rm $linkdest/$f
+	fi
+done
+		
